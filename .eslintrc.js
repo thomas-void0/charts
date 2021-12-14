2e0cc2926c40f1107cb4fe1998b93ec49a02e96c
yangjingyu <yjy15680489038@163.com>
2021-12-14 13:50:45 +0800

    ✨ feat: add render

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,26 @@
+// .eslintrc.js
 module.exports = {
   env: {
     browser: true,
     es2021: true,
+    node: true,
     jest: true,
   },
-  extends: [
-    'airbnb-base',
-  ],
+  extends: ['airbnb-base'],
   parserOptions: {
     ecmaVersion: 13,
     sourceType: 'module',
   },
   rules: {
+    // 关闭 eslint 的如下功能
     'import/prefer-default-export': 0,
+    'no-use-before-define': 0,
+    'no-shadow': 0,
+    'no-restricted-syntax': 0,
+    'no-return-assign': 0,
+    'no-param-reassign': 0,
+    'no-sequences': 0,
+    'no-loop-func': 0,
+    'no-nested-ternary': 0,
   },
-  ignore: [
-    "./__test__/*",
-    "./jest.config.js",
-    "./rollup.config.js"
-  ]
 };
---
+++ src/renderer/renderer.js
@@ -0,0 +1,8 @@
+// export function createRenderer(width, height) {
+//   const context = createContext(width, height);// 创建上下文信息
+
+//   return {
+//     line:
+//   }
+
+// }
